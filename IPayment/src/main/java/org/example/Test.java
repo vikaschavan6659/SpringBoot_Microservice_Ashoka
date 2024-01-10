331b1d3a240b4085d8984dd628e0c240be82503f
vikaschavan12 <vikas.chavan@sumasoft.net>
2024-01-10 12:33:01 +0530

    add code to start engine by P/D

--- IPayment/src/main/java/org/example/Test.java
+++ IPayment/src/main/java/org/example/Test.java
@@ -6,6 +6,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
     public static void main(String[] args) {
         ApplicationContext context= new ClassPathXmlApplicationContext("bean.xml");
-         PetrolEngine petrolEngine=context.getBean(PetrolEngine.class);
+//         PetrolEngine petrolEngine=context.getBean(PetrolEngine.class);
+         DieselEngine dieselEngine=context.getBean(DieselEngine.class);
+//         petrolEngine.startEngine(234);
+         dieselEngine.startEngine(143);
+
+         StartEngineBy startEngineBy=new StartEngineBy(dieselEngine);
+         startEngineBy.statTheEngine(10);
     }
 }
